da151efe4534a1c077c92682dab0f107e5bc4d74
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-04 20:32:28 -0500

    created 'Song' model for 'songs' app. registered 'songs' and rest framework in project settings. installed apps section

--- musiclibrary_project/settings.py
+++ musiclibrary_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'songs',
 ]
 
 MIDDLEWARE = [
@@ -99,7 +101,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
